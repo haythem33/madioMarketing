accc9a0da2097c3a7f571b08f9a8b10ca60b6d3f
haythem33 <esperance191@hotmail.fr>
2020-07-11 12:59:06 +0100

    basename Linking

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,12 @@ import Home  from "../components/home/home"
 import Contact from "../components/contact/contact"
 import About from "../components/about/about";
 import Pricing from "../components/pricing/pricing";
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import {HashRouter , Route, Switch } from 'react-router-dom'
 import detail from "../components/solution/detail";
 export default class router extends React.Component {
     render() {
         return (
-         <Router>
+         <HashRouter basename="/">
              <Switch>
              <Route exact path="/" component={Home} />
              <Route path="/home" component={Home} />
@@ -17,7 +17,7 @@ export default class router extends React.Component {
              <Route path="/pricing" component={Pricing} />
              <Route path="/detail" component={detail} />
              </Switch>
-         </Router>
+         </HashRouter>
         )
     }
 }
