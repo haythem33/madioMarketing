8a5797e3eac25cb3a2d9bd8370c220d0622e8f89
haythem33 <esperance191@hotmail.fr>
2020-07-11 12:29:54 +0100

    Router config for production

--- src/router/router.js
+++ src/router/router.js
@@ -3,18 +3,20 @@ import Home  from "../components/home/home"
 import Contact from "../components/contact/contact"
 import About from "../components/about/about";
 import Pricing from "../components/pricing/pricing";
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import detail from "../components/solution/detail";
 export default class router extends React.Component {
     render() {
         return (
          <Router>
+             <Switch>
              <Route exact path="/" component={Home} />
              <Route path="/home" component={Home} />
              <Route path="/contact" component={Contact} />
              <Route path="/about" component={About} />
              <Route path="/pricing" component={Pricing} />
              <Route path="/detail" component={detail} />
+             </Switch>
          </Router>
         )
     }
