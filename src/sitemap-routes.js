689af249ea44d0f89f00ef87434e8104cdf3410f
haythem33 <esperance191@hotmail.fr>
2020-07-15 22:59:36 +0100

    site map configuration

---
+++ src/sitemap-routes.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Route } from 'react-router';
+
+export default (
+    <Route>
+       <Route path="/"/>
+       <Route path="/home"/>
+       <Route path="/contact"/>
+       <Route path="/about" />
+       <Route path="/pricing"/>
+       <Route path="/detail" />
+       <Route path="/dashboard"/>
+    </Route>
+)
